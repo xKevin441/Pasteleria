d91a7b3008917cf77f4a7b7db9963c79443539bc
Kevin R. Tarqui Mita <126684333+xKevin441@users.noreply.github.com>
2024-05-19 10:08:59 -0400

    Initial commit

---
+++ app/src/main/java/com/edu/pasteleria/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.edu.pasteleria
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button_ofertas = findViewById<Button>(R.id.button_ofertas)
+        val button_catalogo = findViewById<Button>(R.id.button_catalogo)
+        val button_contacto = findViewById<Button>(R.id.button_contactos)
+        val button_salir = findViewById<Button>(R.id.button_salir)
+
+        button_ofertas.setOnClickListener(){
+            val intent = Intent(this@MainActivity, ofertaActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_catalogo.setOnClickListener(){
+            val intent = Intent(this@MainActivity, catalogoActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_contacto.setOnClickListener(){
+            val intent = Intent(this@MainActivity, contactosActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_salir.setOnClickListener(){
+            finish();
+        }
+    }
+}
