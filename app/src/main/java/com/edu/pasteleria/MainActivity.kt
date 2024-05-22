e1f6892ecee62c47dbe024f31c9a8d08fd661dff
Kevin R. Tarqui Mita <126684333+xKevin441@users.noreply.github.com>
2024-05-21 23:23:39 -0400

    Contactos

--- app/src/main/java/com/edu/pasteleria/MainActivity.kt
+++ app/src/main/java/com/edu/pasteleria/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.edu.pasteleria
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,7 +22,8 @@ class MainActivity : AppCompatActivity() {
         }
         val button_ofertas = findViewById<Button>(R.id.button_ofertas)
         val button_catalogo = findViewById<Button>(R.id.button_catalogo)
-        val button_contacto = findViewById<Button>(R.id.button_contactos)
+        val button_contactos = findViewById<Button>(R.id.button_contactos)
+        val button_pedido = findViewById<Button>(R.id.button_pedidos)
         val button_salir = findViewById<Button>(R.id.button_salir)
 
         button_ofertas.setOnClickListener(){
@@ -33,7 +36,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        button_contacto.setOnClickListener(){
+        button_contactos.setOnClickListener(){
+            val intent = Intent(this@MainActivity, contactosActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_pedido.setOnClickListener(){
             val intent = Intent(this@MainActivity, pedidosActivity::class.java)
             startActivity(intent)
         }
